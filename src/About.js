3c1eeb42baae2b81ede26c1b74898fa8b404e924
Ricky Perez <ricky@Rickys-MacBook-Pro.local>
2022-11-17 10:18:01 -0600

    Adding my friend hedgehog

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import sunny from "./resources/sunny.gif"
 
 /* 
   feel free to add/remove any HTML you would like. Just remember that all content
@@ -9,6 +10,7 @@ const About = () => {
   return (
     <div className="app-content">
       <h1>About Us</h1>
+      <img src={sunny} alt="it's always sunny dancer"></img>
       <p>This is a simple demo app for testing Appcues.</p>
       <p>Check out <a href="https://github.com/birbmaaan/appcues_demo_app">the Github repo</a> for directions on how to set up your own.</p>
       <p>The Appcues Toolbox on the right gives you some helpful tools for testing</p>
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import sunny from "./resources/sunny.gif"
+import hedgehog from "./resources/hedgehog.jpg"
 
 /* 
   feel free to add/remove any HTML you would like. Just remember that all content
@@ -11,7 +11,7 @@ const Home = () => {
     <div className="app-content">
       <h1>Welcome Rickcues App</h1>
       <h2>Let's build something.</h2>
-      <img src={sunny} alt="it's always sunny dancing"></img>
+      <img src={hedgehog} alt="it's always sunny in this hedgehog"></img>
     </div>
   );
 };
